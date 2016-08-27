fbb360ff90b05aa33dea0a5dad2d96d3db163235
Diego F. Ruiz S <yego23@gmail.com>
2016-08-27 12:21:37 -0500

    Update models
    Competition
    Video

--- smarttools/web/admin.py
+++ smarttools/web/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Usuario
+from .models import Usuario, Competition, Video
 
 
 @admin.register(Usuario)
-class SportAdmin(admin.ModelAdmin):
-    list_display = ('name','surname','email',)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email',)
+
+
+@admin.register(Competition)
+class CompetitionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'startingDate', 'deadline',)
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'state', 'user_email','competition')
--- smarttools/web/models.py
+++ smarttools/web/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -14,3 +16,28 @@ class Usuario(models.Model):
 
     class Meta:
         ordering = ['name']
+
+class Competition(models.Model):
+    name = models.CharField(max_length=200,null=False)
+    image = models.CharField(max_length=200, null=False)
+    url = models.CharField(max_length=200, null=False)
+    startingDate = models.DateTimeField(null=False)
+    deadline = models.DateTimeField(null=False)
+    description = models.CharField(max_length=200, null=False)
+    user = models.ForeignKey(User, null=False)
+
+    def __str__(self):
+        return self.name
+
+class Video(models.Model):
+    name = models.CharField(max_length=200,null=False)
+    pathOrigin = models.CharField(max_length=200, null=False)
+    pathConverted = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=30, null=False)
+    user_email = models.CharField(max_length=30, null=False)
+    uploadDate = models.DateTimeField(null=False)
+    message = models.CharField(max_length=200, null=False)
+    competition = models.ForeignKey(Competition, null=False)
+
+    def __str__(self):
+        return self.name
