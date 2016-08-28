1efe7ef41bfc7865bb2708b2d23576d103fd29f0
Diego F. Ruiz S <yego23@gmail.com>
2016-08-28 13:21:06 -0500

    Refactor url - to company

--- smarttools/web/urls.py
+++ smarttools/web/urls.py
@@ -5,6 +5,6 @@ from .views import IndexView, RegisterView, HomeView, CompetitionView
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^register/', RegisterView.as_view(), name='register'),
-    url(r'^home/', HomeView.as_view(), name='home'),
+    url(r'^home/(?P<company_name>\w+)', HomeView.as_view(), name='home'),
     url(r'^competition/', CompetitionView.as_view(), name='competition'),
 ]
--- smarttools/web/views.py
+++ smarttools/web/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, TemplateView
-from .models import Usuario
+from .models import Usuario, User
 
 class IndexView (ListView):
     model = Usuario
@@ -12,8 +12,15 @@ class RegisterView (ListView):
     template_name = 'register.html'
 
 class HomeView (ListView):
-    model = Usuario
+    model = User
     template_name = 'homecompetitions.html'
+    context_object_name = 'company'
+
+    def get_queryset(self, **kwargs):
+        company = self.kwargs['company_name']
+        print("---> "+company)
+        queryset = User.objects.filter(username__exact= company)
+        return queryset
 
 class CompetitionView (ListView):
     model = Usuario
