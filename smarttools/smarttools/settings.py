6decf57b3679c3adbbdc872198993434c1f47645
Abimelec Cuesta <acuestap@gmail.com>
2016-08-30 11:24:37 -0500

    commit setting
    Soluciona a conflicto antes de hacer el emerge con develop-a

--- smarttools/smarttools/settings.py
+++ smarttools/smarttools/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY_SMARTTOOLS']
-
+#SECRET_KEY = os.environ['SECRET_KEY_SMARTTOOLS']
+SECRET_KEY = 'qrjk_0l##y&+@+sdju!g2i1l@qwb(b=^h5)gp3)ihj(+=l2ct$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
