564dbdc055e7d386e60dfb91e7228e7b43a229ae
Abimelec Cuesta <acuestap@gmail.com>
2016-08-30 12:00:42 -0500

    Ajustes en web/urls

--- smarttools/web/urls.py
+++ smarttools/web/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .views import WebIndexView, HomeView, CompetitionView, AddVideoView
 
 urlpatterns = [
-    url(r'^$', login_required(WebIndexView.as_view()), name='web_index'),
+    url(r'^web$', login_required(WebIndexView.as_view()), name='web_index'),
     url(r'^home/(?P<company_name>\w+)', HomeView.as_view(), name='home'),
     url(r'^competition/(?P<company_name>\w+)', CompetitionView.as_view(), name='competition'),
     url(r'^video/add', AddVideoView.as_view(), name='addvideo'),
